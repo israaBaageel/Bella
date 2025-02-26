00b8275eb08e1eb0c69d8c15723a0c744c5ffd9f
Sarah <sarahkkb@hotmail.com>
2025-02-26 03:15:37 +0300

    profile first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:test/auth/login_or_signUp.dart';
 import 'package:test/pages/forgot_pass.dart';
 import 'package:test/pages/otp.dart';
+import 'package:test/pages/homePage.dart';
+import 'package:test/pages/profile.dart';
 import 'package:test/theme/dark_mode.dart';
 import 'package:test/theme/light_mode.dart';
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home : LoginOrSignUp(),
+      home : HomePage(),
       theme: lightMode,
       darkTheme: darkMode,
 
